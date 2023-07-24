c11be1c6e3e3afb9e0f05ac13e6ae6c4689d98d0
pagar22 <aaryanpagar@hotmail.com>
2023-07-24 19:06:15 +0200

    feat: basic expo app w sendbird

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
+import { initializeApp } from "firebase/app";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SendbirdUIKitContainer } from '@sendbird/uikit-react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// internal
+import {FileService, NotificationService, ClipboardService, MediaService} from "./src/services"
+
+const fireBaseConfigDevelopment = {
+  apiKey: "AIzaSyBoPxATRJMVmYLLYkQfmZSRIOSDeVkZxiM",
+  authDomain: "development-326115.firebaseapp.com",
+  projectId: "development-326115",
+  storageBucket: "development-326115.appspot.com",
+  messagingSenderId: "1041408332003",
+  appId: "1:1041408332003:web:99f959702b1880af9f35d6",
+  measurementId: "G-J9RNLX71TV",
+};
+export const firebaseApp = initializeApp(fireBaseConfigDevelopment)
 
 export default function App() {
+
   return (
+    <SendbirdUIKitContainer
+    appId={'APP_ID'}
+    chatOptions={{ localCacheStorage: AsyncStorage }}
+    platformServices={{
+        file: FileService,
+        // notification: NotificationService(firebaseApp),
+        clipboard: ClipboardService,
+        media: MediaService,
+    }}
+>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </SendbirdUIKitContainer>
+
   );
 }
 
