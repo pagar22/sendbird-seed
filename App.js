588888ebd4bd5c291d5fbfed66bb4cc0f9768d25
pagar22 <aaryanpagar@hotmail.com>
2023-07-26 10:14:01 +0200

    feat: responsive breakpoints

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
+import SendbirdApp from "@sendbird/uikit-react/App";
+import "@sendbird/uikit-react/dist/index.css";
+// internal
 import { SENDBIRD_APP_ID } from "./secrets/secrets";
 import { Navigation } from "./src/navigation/app.navigator";
+import { View } from "react-native";
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <SendbirdApp
+        appId={SENDBIRD_APP_ID}
+        userId={"pagar22"}
+        breakpoint={true}
+      />
+    </View>
+  );
 }
